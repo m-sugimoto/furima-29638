351ea539844f84075a7fbe3d4c264bed4583b83e
杉本護 <sugimotomamoru@sugimotorunoAir.AirPort>
2021-01-15 16:07:31 +0900

    商品情報編集機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only:[:new, :create] 
+before_action :authenticate_user!, only:[:new, :create, :edit, :update] 
+before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,15 +21,18 @@ before_action :authenticate_user!, only:[:new, :create]
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-   @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user_id 
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != @item.user_id 
+      redirect_to root_path
+    end
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -39,6 +43,10 @@ before_action :authenticate_user!, only:[:new, :create]
   private
   def item_params
     params.require(:item).permit(:image, :commodity, :explan, :category_id, :status_id, :prefecture_id, :postage_id, :sendingday_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
 
